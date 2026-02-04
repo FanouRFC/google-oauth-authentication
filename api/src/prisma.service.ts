accffafedd0c0a729e102bbff0de6a4d9b5d1f02
unknown <fanouomega@gmail.com>
2026-02-04 20:39:59 +0300

    fix: fix connection string

--- api/src/prisma.service.ts
+++ api/src/prisma.service.ts
@@ -5,7 +5,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaPg({ url: process.env.PRISMA_DIRECT_TCP_URL });
+    const adapter = new PrismaPg({ connectionString : process.env.DATABASE_URL });
     super({ adapter });
   }
 }
