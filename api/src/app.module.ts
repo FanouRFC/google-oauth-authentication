aeeeb7ada4eae41a80c8a12510b419498c53118a
unknown <fanouomega@gmail.com>
2026-02-04 20:18:55 +0300

    fix: importation prisma service

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule],
   controllers: [AppController],
-  providers: [AppService, ],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
