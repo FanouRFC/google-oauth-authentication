8f9422a582bd5e129eedef263e2e44d1b4b5405d
unknown <fanouomega@gmail.com>
2026-02-04 20:14:34 +0300

    feat: installation prisma et prisma client

---
+++ api/src/prisma.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from './generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaPg({ url: process.env.PRISMA_DIRECT_TCP_URL });
+    super({ adapter });
+  }
+}
