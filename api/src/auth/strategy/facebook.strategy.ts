af00668dde8d4d06902bfe24469d74828dbe6285
unknown <fanouomega@gmail.com>
2026-02-07 22:22:52 +0300

    fix: mise a jour strategy google => ajout champ photo et mail

--- api/src/auth/strategy/facebook.strategy.ts
+++ api/src/auth/strategy/facebook.strategy.ts
@@ -10,14 +10,15 @@ export class FacebookStrategy extends PassportStrategy(Strategy, "facebook"){
             clientID: process.env.FACEBOOK_APP_ID!,
             clientSecret: process.env.FACEBOOK_APP_SECRET!,
             callbackURL: "http://localhost:3000/auth/facebook-redirect",
-            profileFields: ["id", "emails", "name", "displayName"],
+            scope: ['email'],
+            profileFields: ["id", "email","name", "displayName", "photos"],
         })
     }
     validate(accessToken:string, refreshToken:string, profile:any): any {
         return {
             accessToken,
             refreshToken,
-            profile,
+            profile
         }
     }
 }
