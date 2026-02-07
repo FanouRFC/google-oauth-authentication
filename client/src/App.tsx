e38e75ba1b316fcdd224ecd688ed174a244e7a8c
unknown <fanouomega@gmail.com>
2026-02-07 20:21:54 +0300

    feat: installation et configuration react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,20 @@
 import "./App.css";
 import Login from "./pages/login";
+import { BrowserRouter, Routes, Route } from "react-router";
 import UserList from "./pages/userList";
+import Dashboard from "./pages/dashboard";
+import NotFoundRoute from "./pages/404NotFound";
 
 function App() {
   return (
-    <>
-      {/* <Login /> */}
-      <UserList />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<UserList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFoundRoute />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/404NotFound.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundRoute() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <p className="text-7xl font-bold">404 Not Found</p>
+    </div>
+  );
+}
