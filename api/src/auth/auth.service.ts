c2ccdcf6f70946bbb3f3ef9ba4a1a3b840f9cdc8
unknown <fanouomega@gmail.com>
2026-02-07 22:23:57 +0300

    feat: ajout dans BD les informations apres logins de facebook et google

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,22 +1,68 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-    googleLogin(req) {
+  constructor(private UserService : UserService){}
+    async googleLogin(req) {
     if (!req.user) {
       return 'No user from google';
     }
 
+    const {firstName, lastName, picture, email} = req.user
+
+    var data = {
+      email,
+      name: firstName + " " + lastName,
+      picture,
+      provider: "google"
+    }
+  
+  var EmailExist = await this.UserService.checkEmailExist(email, "google")
+  if(!EmailExist){
+    await this.UserService.add(data)
+     return {
+       message: 'User information from google',
+       user: req.user,
+     };
+  }
+  else{
     return {
-      message: 'User information from google',
-      user: req.user,
-    };
+       message: 'COMPTE DEJA EXISTANT AVEC LES RENSEIGNEMENTS SUIVANTS !!! ',
+       user: req.user,
+     };
+  }
   }
 
-  facebookLogin(req){
+  async facebookLogin(req){
     if(!req.user){
         return "No user found"
     }
-    return req.user
+  
+  const {profile} = req.user
+  const {displayName} = profile
+  const picture = profile.photos[0].value
+  const email = profile.emails[0].value
+    var data = {
+      email,
+      name: displayName,
+      picture,
+      provider: "facebook"
+    }
+
+  var EmailExist = await this.UserService.checkEmailExist(email, "facebook")
+  if(!EmailExist){
+    await this.UserService.add(data)
+     return {
+       message: 'User information from google',
+       user: req.user,
+     };
+  }
+  else{
+    return {
+       message: 'COMPTE DEJA EXISTANT AVEC LES RENSEIGNEMENTS SUIVANTS !!! ',
+       user: req.user,
+     };
+  }
   }
 }
