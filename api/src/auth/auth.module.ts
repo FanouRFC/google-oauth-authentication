e22aab452a245b2e3231153ceca271c83cf7b7a0
unknown <fanouomega@gmail.com>
2026-02-07 22:23:27 +0300

    fix: importer userModule

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -3,8 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { GoogleStrategy } from './strategy/google.strategy';
 import { FacebookStrategy } from './strategy/facebook.strategy';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports:[UserModule,],
   controllers: [AuthController],
   providers: [AuthService, GoogleStrategy, FacebookStrategy]
 })
