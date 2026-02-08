21d9acbe901d5b83162ebd1b69bbe8d982c4b095
unknown <fanouomega@gmail.com>
2026-02-08 20:23:14 +0300

    ajout route authCallBack

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,19 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import UserList from "./pages/userList";
 import Dashboard from "./pages/dashboard";
 import NotFoundRoute from "./pages/404NotFound";
+import { useEffect, useState } from "react";
+import AuthCallBack from "./pages/authCallBack";
 
 function App() {
+  const [userInfo, setUserInfo] = useState<{ id: number }>();
+  useEffect(() => {
+    var token = localStorage.getItem("token");
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/admin" element={<UserList />} />
+        <Route path="/auth" element={<AuthCallBack />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFoundRoute />} />
