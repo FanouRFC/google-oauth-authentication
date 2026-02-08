cefc04489436d0c11fba1c7d2703338724651553
unknown <fanouomega@gmail.com>
2026-02-08 20:24:52 +0300

    feat(user): ajout apu getById

--- client/src/api/userApi.ts
+++ client/src/api/userApi.ts
@@ -8,6 +8,7 @@ type userType = {
 export const userApi = {
     getAll : () => {return axiosInstance.get("/user")},
     getBySearch: (search: string)=>{return axiosInstance.get(`/user/${search}`)},
+    getById: (id: number)=>{return axiosInstance.get(`/user/v1/${id}`)},
     add: (data: userType)=>{return axiosInstance.post("/user", data)},
     delete: (id: number)=>{return axiosInstance.delete(`/user/${id}`)},
     update: (id: number, data: userType)=>{return axiosInstance.patch(`/user/${id}`, data)}
