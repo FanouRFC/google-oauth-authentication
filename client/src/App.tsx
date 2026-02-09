7d4ceeb83a995768b7358ddd7ffd41e73800de44
unknown <fanouomega@gmail.com>
2026-02-09 20:28:44 +0300

    fix: Mise a jour route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,40 @@
 import "./App.css";
 import Login from "./pages/login";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import UserList from "./pages/userList";
 import Dashboard from "./pages/dashboard";
 import NotFoundRoute from "./pages/404NotFound";
 import { useEffect, useState } from "react";
 import AuthCallBack from "./pages/authCallBack";
+import { axiosInstance } from "./api/axiosConfig";
+import Register from "./pages/register";
 
 function App() {
   const [userInfo, setUserInfo] = useState<{ id: number }>();
   useEffect(() => {
     var token = localStorage.getItem("token");
+    async function getInfo() {
+      try {
+        const req = await axiosInstance.get("/auth/me", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setUserInfo(req.data.userId);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    if (token) {
+      getInfo();
+    }
   }, []);
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/auth" replace />} />
         <Route path="/admin" element={<UserList />} />
         <Route path="/auth" element={<AuthCallBack />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFoundRoute />} />
       </Routes>
