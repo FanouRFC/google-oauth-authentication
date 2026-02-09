63c3e272d87ff3570181a88fa2cf8e619f6b097e
unknown <fanouomega@gmail.com>
2026-02-09 20:14:14 +0300

    fix: ajout props id, onChange function

--- client/src/components/custom/input.tsx
+++ client/src/components/custom/input.tsx
@@ -1,13 +1,22 @@
 import type { LucideIcon } from "lucide-react";
 import { Input } from "../ui/input";
+import type { ChangeEvent } from "react";
 
 type CInputProps = {
+  id?: string;
   Icone?: LucideIcon;
   type: React.HTMLInputTypeAttribute | undefined;
   placeholder: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 };
 
-export default function CInput({ Icone, type, placeholder }: CInputProps) {
+export default function CInput({
+  Icone,
+  type,
+  placeholder,
+  id,
+  onChange,
+}: CInputProps) {
   return (
     <div className="relative">
       {Icone && (
@@ -17,7 +26,14 @@ export default function CInput({ Icone, type, placeholder }: CInputProps) {
         />
       )}
       <div className="absolute l-0 -translate-y-1/2 t-1/2"></div>
-      <Input type={type} placeholder={placeholder} className="ps-10" />
+      <Input
+        type={type}
+        placeholder={placeholder}
+        className="ps-10"
+        id={id}
+        onChange={onChange}
+        required
+      />
     </div>
   );
 }
