5444d1962e5cf09a0610fa4f6a7e88a7389f2095
unknown <fanouomega@gmail.com>
2026-02-09 20:17:37 +0300

    fix: ajout condition tokenL(venant simple login)

--- client/src/pages/authCallBack.tsx
+++ client/src/pages/authCallBack.tsx
@@ -1,18 +1,21 @@
 import { useEffect } from "react";
-import { useNavigate, useSearchParams } from "react-router";
+import { useLocation, useNavigate, useSearchParams } from "react-router";
 
 export default function AuthCallBack() {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+  const location = useLocation();
+
   useEffect(() => {
+    const tokenL = location?.state?.token;
     const token: string | null = searchParams.get("token");
-    if (token) {
-      localStorage.setItem("token", token);
+    if (token || tokenL) {
+      localStorage.setItem("token", token ?? tokenL);
       navigate("/dashboard");
     } else {
       navigate("/login");
     }
-  }, [searchParams, navigate]);
+  }, [searchParams, navigate, location.state]);
 
   return (
     <div className="flex items-center h-screen ">
